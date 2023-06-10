b7defc8baded98aa403d6b6a8945eb48254591c6
aunglynn550 <aunglynn550@gmail.com>
2023-06-10 10:09:57 +0630

    real_world_CacheInterface

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+require_once 'Application.php';
+require_once 'FileCache.php';
+require_once 'RedisCache.php';
+
+//only change happens here
+
+$app = new Application(
+    new FileCache()
+    //new RedisCache()
+);
+
+$HTML = "...";
+$app->cache->set('home-page',$HTML);
+$app->cache->get('home-page');
+$app->cache->invalidate('home-page');
